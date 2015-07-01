e1e00984a1989e74cb376c20b057c93604f916e6
Hunor Kovács <hunor.kovacs@ft.com>
2015-07-01 18:26:56 +0300

    Trying to source from queue, not successful yet.

--- src/main/scala/com/hunorkovacs/riptube/Balancing.scala
+++ src/main/scala/com/hunorkovacs/riptube/Balancing.scala
@@ -1,9 +1,14 @@
 package com.hunorkovacs.riptube
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, Actor, ActorSystem}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import com.hunorkovacs.riptube.Generator.Generate
 import org.slf4j.LoggerFactory
+
+import scala.collection.immutable.Queue
+import scala.io.StdIn
+import scala.concurrent.duration._
 
 object Balancing extends App {
 
@@ -13,13 +18,19 @@ object Balancing extends App {
   implicit private val implicitEc = actorSystem.dispatcher
   implicit private val materializer = ActorMaterializer()
 
+  private val queue = Queue[Int]()
+  private val generator = actorSystem.actorOf(Generator.props(queue))
+  private val generating = actorSystem.scheduler.schedule(0 seconds, 200 millis, generator, Generate)
+
   private val slowFlow = Flow[Int].map(slowProcessor)
   private val balancer = balance(slowFlow, 3)
 
-  Source[Int](1 to 10)
+  Source[Int](queue)
     .via(balancer)
     .runForeach(e => logger.debug(s"element $e"))
-    .onComplete(_ => actorSystem.shutdown())
+
+  StdIn.readLine()
+  actorSystem.shutdown()
 
   def balance[In, Out](worker: Flow[In, Out, Any], workerCount: Int): Flow[In, Out, Unit] = {
     import FlowGraph.Implicits._
@@ -42,4 +53,24 @@ object Balancing extends App {
     Thread.sleep(1000)
     i * 10
   }
+}
+
+class Generator(queue: Queue[Int]) extends Actor {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+  private var i = 0
+
+  override def receive = {
+    case Generate =>
+      logger.debug(s"enqueuing $i...")
+      queue.enqueue(i)
+      i += 1
+  }
+}
+
+object Generator {
+
+  case object Generate
+
+  def props(queue: Queue[Int]) = Props(classOf[Generator], queue)
 }
