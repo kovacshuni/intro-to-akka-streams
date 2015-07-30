b3ef899e6d9746dba082d4d1f6eb0e640ed6a6b5
Hunor Kovács <hunor.kovacs@ft.com>
2015-07-30 15:58:50 +0300

    Slowing consumer

---
+++ src/main/scala/com/hunorkovacs/introtoakkastreams/Intro.scala
@@ -0,0 +1,28 @@
+package com.hunorkovacs.introtoakkastreams
+
+import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.io.StdIn
+
+object Intro extends App {
+
+  implicit private val sys = ActorSystem("intro-system")
+  implicit private val mat = ActorMaterializer()
+  private val influx = sys.actorOf(Props[Influx], "influx")
+
+  private val producer = new Producer(influx, sys)
+  private val consumer = new SlowingConsumer(influx, sys)
+
+  Source(() => Iterator.continually[Int](producer.produce()))
+    .runForeach(consumer.consume)
+
+  StdIn.readLine()
+  Await.ready(Http().shutdownAllConnectionPools(), 4 seconds)
+  sys.shutdown()
+  sys.awaitTermination()
+}
---
+++ src/main/scala/com/hunorkovacs/introtoakkastreams/Measured.scala
@@ -0,0 +1,46 @@
+package com.hunorkovacs.introtoakkastreams
+
+import akka.actor.{Inbox, ActorSystem, ActorRef}
+import com.hunorkovacs.introtoakkastreams.Influx.Metric
+
+abstract class Measured(private val influx: ActorRef, private val sys: ActorSystem) {
+
+  protected val inbox = Inbox.create(sys)
+}
+
+trait Consumer {
+
+  def consume(i: Int): Unit
+}
+
+class NormalConsumer(influx: ActorRef, sys: ActorSystem) extends Measured(influx, sys) with Consumer {
+
+  override def consume(i: Int) = {
+    Thread.sleep(100)
+    val now = System.currentTimeMillis
+    inbox.send(influx, Metric(s"consumer-1 value=$i $now", now))
+  }
+}
+
+class SlowingConsumer(influx: ActorRef, sys: ActorSystem) extends Measured(influx, sys) with Consumer {
+
+  private var t = 100
+
+  override def consume(i: Int) = {
+    Thread.sleep(t)
+    if (t < 300) t += 1
+    val now = System.currentTimeMillis
+    inbox.send(influx, Metric(s"consumer-2 value=$i $now", now))
+  }
+}
+
+class Producer(influx: ActorRef, sys: ActorSystem) extends Measured(influx, sys) {
+
+  def produce() = {
+    Thread.sleep(50)
+    val i = 0
+    val now = System.currentTimeMillis
+    inbox.send(influx, Metric(s"producer value=$i $now", now))
+    i
+  }
+}
