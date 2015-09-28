fe52c1287296184608016501bedaba9fe00a5ac2
Hunor Kovács <hunor.kovacs@ft.com>
2015-09-29 01:34:43 +0300

    Fast producer slow consumer in one class.

--- src/main/scala/com/hunorkovacs/introtoakkastreams/Intro.scala
+++ src/main/scala/com/hunorkovacs/introtoakkastreams/Intro.scala
@@ -1,11 +1,10 @@
 package com.hunorkovacs.introtoakkastreams
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, Inbox, ActorSystem}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpMethods}
-import akka.http.scaladsl.model.HttpMethods.POST
-import akka.stream.{OverflowStrategy, ActorMaterializer}
-import akka.stream.scaladsl._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import com.hunorkovacs.introtoakkastreams.Influx.Metric
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -15,17 +14,28 @@ object Intro extends App {
 
   implicit private val sys = ActorSystem("intro")
   implicit private val mat = ActorMaterializer()
-  private val influx = sys.actorOf(Influx.props, "influx")
 
-  val producer = new Producer(influx, sys)
-  val pool = Http().cachedHostConnectionPool[Int]("localhost", 8080)
+  private val influx = sys.actorOf(Props[Influx], "influx")
+  private val inbox = Inbox.create(sys)
 
-  Source(() => Iterator.continually[Int](producer.produce()))
-    .map(i => HttpRequest(method = POST, uri = "/consume", entity = i.toString) -> i)
-    .via(pool)
-    .runWith(Sink.ignore)
+  val source = Source(() => Iterator.continually{
+    Thread.sleep(50)
+    val i = 0
+    val now = System.currentTimeMillis
+    inbox.send(influx, Metric(s"producer $i $now", now))
+    i
+  })
+  val sink = Sink.foreach[Int] { i =>
+//    Thread.sleep(100)
+//    val now = System.currentTimeMillis
+//    inbox.send(influx, Metric(s"consumer $i $now", now))
+  }
 
-  StdIn.readLine()
+  val runnableFlow = source.to(sink)
+
+  runnableFlow.run()
+
+  StdIn.readLine
   Await.ready(Http().shutdownAllConnectionPools(), 2 seconds)
   sys.shutdown()
   sys.awaitTermination()
