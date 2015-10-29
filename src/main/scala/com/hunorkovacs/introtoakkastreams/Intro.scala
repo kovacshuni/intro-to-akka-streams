5cafbfeb5eaafc4f0f502b46acbd7361152b38f8
Hunor Kovács <hunor.kovacs@ft.com>
2015-10-29 15:16:34 +0200

    Clearing after presentation.

--- src/main/scala/com/hunorkovacs/introtoakkastreams/Intro.scala
+++ src/main/scala/com/hunorkovacs/introtoakkastreams/Intro.scala
@@ -1,11 +1,9 @@
 package com.hunorkovacs.introtoakkastreams
 
-import akka.actor.{Inbox, Props, ActorSystem}
-import akka.stream.{OverflowStrategy, ActorMaterializer}
+import akka.actor.{ActorSystem, Props}
 import akka.stream.scaladsl._
-import com.hunorkovacs.introtoakkastreams.Influx.Metric
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 
-import scala.concurrent.Future
 import scala.io.StdIn
 
 object Intro extends App {
@@ -16,24 +14,12 @@ object Intro extends App {
   private val influx = sys.actorOf(Props[Influx])
 
   private val producer = new Producer(influx, sys)
-  private val consumers = List(new NormalConsumer(influx, sys))
-  private val consumerFlow = consumers.map(c => Flow[Int].map(i => c.consume(i)))
-  private val balancer = balance(consumerFlow)
 
   Source(() => Iterator.continually(producer.produce))
     .buffer(100, OverflowStrategy.backpressure)
-    .via(balancer)
     .runWith(Sink.ignore)
 
   StdIn.readLine()
   sys.shutdown()
   sys.awaitTermination()
-
-
-  val v1 = Future()
-  v2 = Future(b(v88))
-  r = Future(c(v2))
-
-
-
 }
